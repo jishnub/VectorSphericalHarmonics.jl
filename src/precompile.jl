acddbd0ed0fa9a883159f1447cd60e250a88e39b
jishnub <jishnub@users.noreply.github.com>
2021-05-23 21:32:19 +0400

    precompile directives for VSHCache

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,1 +1,5 @@
 precompile(genspharm, (Int, Int, Float64, Float64))
+for LT in [ZeroTo{false}, UnitRange{Int}], MT in [ZeroTo{true}, FullRange{true}, UnitRange{Int}]
+    precompile(VSHCache, (Type{Float64}, ML{LT, MT}))
+    precompile(VSHCache, (Type{Float64}, LM{LT, MT}))
+end
