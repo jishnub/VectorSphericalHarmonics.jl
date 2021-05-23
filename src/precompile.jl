6339d833588e2c307ff53c7450b9dd95c6f397eb
jishnub <jishnub@users.noreply.github.com>
2021-05-23 21:37:31 +0400

    precompile directives for VSH

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,4 +2,8 @@ precompile(genspharm, (Int, Int, Float64, Float64))
 for LT in [ZeroTo{false}, UnitRange{Int}], MT in [ZeroTo{true}, FullRange{true}, UnitRange{Int}]
     precompile(VSHCache, (Type{Float64}, ML{LT, MT}))
     precompile(VSHCache, (Type{Float64}, LM{LT, MT}))
+    for YT in [PB, Hansen, Irreducible], B in [Cartesian, Polar, SphericalCovariant, HelicityCovariant]
+        precompile(VSHCache, (Type{Float64}, YT, B, ML{LT, MT}))
+        precompile(VSHCache, (Type{Float64}, YT, B, LM{LT, MT}))
+    end
 end
